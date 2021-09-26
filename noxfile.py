1d3ddf7270540d6b7b2eb9e8e01789bcb956fd8c
Ross Moffat <rossmoffat82@gmail.com>
2021-09-26 20:25:56 +0100

    updated to local pre-commit repos

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,16 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
 
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "safety", "mypy", "pytype", "tests"
 
 locations = "src", "tests", "noxfile.py"
 
 
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -23,7 +25,7 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python=["3.9"])
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "coverage", "pytest", "pytest-cov", "pytest-mock")
@@ -31,11 +33,12 @@ def tests(session):
 
 
 @nox.session(python=["3.9"])
-def lint(session):
+def lint(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(
         session,
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
@@ -45,14 +48,14 @@ def lint(session):
 
 
 @nox.session(python="3.9")
-def black(session):
+def black(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python="3.9")
-def safety(session):
+def safety(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -67,8 +70,16 @@ def safety(session):
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-# @nox.session(python=["3.9"])
-# def mypy(session):
-#    args = session.posargs or locations
-#    install_with_constraints(session, "mypy")
-#    session.run("mypy", *args)
+@nox.session(python=["3.9"])
+def mypy(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
+@nox.session(python=["3.9"])
+def pytype(session: Session) -> None:
+    """Run the static type checker."""
+    args = session.posargs or ["--disable=import-error", *locations]
+    install_with_constraints(session, "pytype")
+    session.run("pytype", *args)
--- src/lconrg/factors.py
+++ src/lconrg/factors.py
@@ -1,2 +1,21 @@
-for i in range(5):  # this comment has too many spaces
-    print(i)  # this line has 6 space indentation.
+def present_value(base_year: int, discount_rate: float, no_of_years: int) -> dict:
+    """
+    Calculates a list of discount rates by year
+    Parameters
+    --------------
+    base_year : int
+        The base year for the calculation.
+    discount_rate : float
+        The percentage discount rate.
+    no_of_years : int
+        The number of years from the base year to be calculated.
+    Returns
+    -------------
+    out : dict
+        Dict of Present Value discount factors from base year to end year.
+    """
+
+    return {
+        i: 1 / ((1 + discount_rate) ** (i - base_year))
+        for i in range(base_year, base_year + no_of_years)
+    }
