7e1a6fd2b3aee2cc658e915e26233164a2beabfa
Ross Moffat <rossmoffat82@gmail.com>
2021-09-26 20:48:02 +0100

    Updated docstring to google style

--- src/lconrg/factors.py
+++ src/lconrg/factors.py
@@ -1,20 +1,16 @@
 def present_value(base_year: int, discount_rate: float, no_of_years: int) -> dict:
-    """
-    Calculates a list of discount rates by year
-    Parameters
-    --------------
-    base_year : int
-        The base year for the calculation.
-    discount_rate : float
-        The percentage discount rate.
-    no_of_years : int
-        The number of years from the base year to be calculated.
-    Returns
-    -------------
-    out : dict
+    """Return dict of discount rates.
+
+    Calculates annual discount rates and returns a dict.
+
+    Args:
+        base_year: The base year for the calculation.
+        discount_rate: The percentage discount rate.
+        no_of_years: The number of years from the base year to be calculated.
+
+    Returns:
         Dict of Present Value discount factors from base year to end year.
     """
-
     return {
         i: 1 / ((1 + discount_rate) ** (i - base_year))
         for i in range(base_year, base_year + no_of_years)
