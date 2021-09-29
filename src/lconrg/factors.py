acc0c8ddcce7a72197c060a068ba44e780925edb
Ross Moffat <rossmoffat82@gmail.com>
2021-09-29 20:35:59 +0100

    Playing around with Numpy implementation

--- src/lconrg/factors.py
+++ src/lconrg/factors.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+
+# %%
 def present_value(base_year: int, discount_rate: float, no_of_years: int) -> dict:
     """Return dict of discount rates.
 
@@ -15,3 +19,19 @@ def present_value(base_year: int, discount_rate: float, no_of_years: int) -> dic
         i: 1 / ((1 + discount_rate) ** (i - base_year))
         for i in range(base_year, base_year + no_of_years)
     }
+
+
+# TODO: Change to Numpy implementation np.isin may be the answer
+
+# %%
+
+df = present_value(2025, 0.133, 40)
+# %%
+
+result = df.items()
+data = list(result)
+
+# %%
+years = np.array(range(2020, 2035, 1))
+# %%
+data[np.isin(data[:, 0], years)]
