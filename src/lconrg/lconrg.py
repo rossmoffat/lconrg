3dbe115732238d41cc701511c6b905d6dde30a5a
Ross Moffat <rossmoffat82@gmail.com>
2022-02-19 11:02:11 +0000

    PriceForecast dunderinit and build forecast method

--- src/lconrg/lconrg.py
+++ src/lconrg/lconrg.py
@@ -33,9 +33,10 @@ class PriceForecast:
         self,
         commodity: str,
         base_year: int,
-        price_forecast: Optional[dict],
-        flat_price: Optional[float],
-        yrs_duration: Optional[int],
+        price_forecast: Optional[dict] = None,
+        flat_price: Optional[float] = None,
+        start_yr: Optional[int] = None,
+        end_yr: Optional[int] = None,
     ) -> None:
         """Creates new instance of Price Forecast object.
 
@@ -44,10 +45,36 @@ class PriceForecast:
             base_year (int): _description_
             price_forecast (Optional[dict]): _description_
             flat_price (Optional[float]): _description_
-            yrs_duration (Optional[int]): _description_
+            start_yr (Optional[int]): _description_
+            end_yr (Optional[int]): _description_
         """
         self.commodity = commodity
         self.base_yr = base_year
+        if (price_forecast is None) & (flat_price is None):
+            raise AttributeError(
+                "You need to provide either a flat price, or a price forecast dict."
+            )
+        if price_forecast is not None:
+            self.price_forecast = price_forecast
+        else:
+            self.price_forecast = self.build_price_forecast(
+                flat_price, start_yr, end_yr
+            )
+
+    def build_price_forecast(
+        self, flat_price: float, start_yr: int, end_yr: int
+    ) -> dict:
+        """_summary_.
+
+        Args:
+            flat_price (float): _description_
+            start_yr (int): _description_
+            end_yr (int): _description_
+
+        Returns:
+            dict: _description_
+        """
+        return {year: flat_price for year in range(start_yr, end_yr + 1)}
 
 
 def present_value_factor(
