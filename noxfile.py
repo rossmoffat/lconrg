28edb2a436f64df01c8ed8f555f7184edb6589ee
Ross Moffat <rossmoffat82@gmail.com>
2022-11-10 21:41:50 +0000

    added sphinx session to nox.

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ from nox.sessions import Session
 
 package = "lconrg"
 nox.options.sessions = "lint", "safety", "mypy", "pytype", "tests"
-locations = "src", "tests", "noxfile.py"
+locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
@@ -100,3 +100,10 @@ def xdoctest(session: Session) -> None:
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "xdoctest")
     session.run("python", "-m", "xdoctest", package, *args)
+
+
+@nox.session(python="3.9")
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    install_with_constraints(session, "sphinx")
+    session.run("sphinx-build", "docs", "docs/_build")
