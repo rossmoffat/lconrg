3b7119515767c1cb2f94ae6736cd385cfcb59713
Ross Moffat <rossmoffat82@gmail.com>
2023-10-21 21:43:22 +0100

    sphinx conf update

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@ project = "LCONRG"
 author = "Ross Moffat"
 copyright = f"2022, {author}"
 extensions = [
-    "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
