4650f2a5f7418965aa9bff199e381ec325a84e37
Ross Moffat <rossmoffat82@gmail.com>
2023-11-04 09:19:47 +0000

    updated test_verison

--- tests/test_lconrg.py
+++ tests/test_lconrg.py
@@ -14,7 +14,7 @@ from lconrg.lconrg import (
 
 def test_version():
     """Tests the version number is accurate."""
-    assert __version__ == "2023.11.3"
+    assert __version__ == "2023.11.4"
 
 
 @pytest.fixture
